7a031f6a887a57d3d7c830d06c860790ca8ea880
yanaazkha68-cmyk <yanaazkha68@gmail.com>
2026-09-17 23:19:59 +0700

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,8 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'com.ternakpro.app',
-  appName: 'TERNAKPRO',
-  webDir: 'dist',
-  bundledWebRuntime: false
-};
-export default config;
